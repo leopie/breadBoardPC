99059012254b518bcd9142c2ff8728d1eb07c1e5
leonardo pierangelini <leonardo.pierangelini@gmail.com>
2024-03-10 17:51:04 +0100

    Wrote code for displaying letter H

--- blink.s
+++ blink.s
@@ -1,16 +1,71 @@
+PORTB = $6000
+DDRB = $6002
+
+PORTA = $6001
+DDRA = $6003
+
+E  = %1000000
+RW = %0100000
+RS = %0010000
+
   .org $8000
 
 reset:
-  lda #$ff
-  sta $6002
+  lda #%11111111 ; SET ALL PINS ON PORT B TO OUTPUT
+  sta DDRB
 
-  lda #$50
-  sta $6000
+  lda #%11100000 ; SET TOP 3 PIN ON PORT A TO OUTPUT
+  sta DDRA
+
+  lda #%00111000 ; SET 8-bit MODE; 2-line DISPLAY; 5x8 FONT
+  sta PORTB
+
+  lda #0	 ; CLEAR RS/RW/E bits
+  sta PORTA
+
+  lda #E	 ; TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
+  sta PORTA
+
+  lda #0         ; CLEAR RS/RW/E bits
+  sta PORTA
+
+  lda #%00001110 ; SET DISPLAY ON, SET CURSOR ON, SET CURSOR-BLINKING OFF 
+  sta PORTB
+
+  lda #0         ; CLEAR RS/RW/E bits
+  sta PORTA
+
+  lda #E         ; TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
+  sta PORTA
+
+  lda #0         ; CLEAR RS/RW/E bits
+  sta PORTA
+
+  lda #%00000110 ; SET DISPLAY TO SHIFT THE CURSOR TO THE RIGHT, SET TO NOT SHIFT DISPLAY
+  sta PORTB
+
+  lda #RS         ; SET RS AND CLEAR RW/E bits
+  sta PORTA
+
+  lda #(RS | E)         ; KEEP RS SET AND TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
+  sta PORTA
+
+  lda #RS         ; SET RS AND CLEAR RW/E bits
+  sta PORTA
+
+  lda #"H"	 ; WRITE H 
+  sta PORTB
+
+  lda #0         ; CLEAR RS/RW/E bits
+  sta PORTA
+
+  lda #E         ; TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
+  sta PORTA
+
+  lda #0         ; CLEAR RS/RW/E bits
+  sta PORTA
 
 loop:
-  ror
-  sta $6000
-
   jmp loop
 
   .org $fffc
