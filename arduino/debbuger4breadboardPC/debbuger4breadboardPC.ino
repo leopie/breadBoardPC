8a6480bd39e76e164a195fbc8bad7508cb22d47f
leonardo.pierangelini <leonardo.pierangelini@gmail.com>
2024-03-31 09:28:29 +0200

    Added RAM and code routine

--- arduino/debbuger4breadboardPC/debbuger4breadboardPC.ino
+++ arduino/debbuger4breadboardPC/debbuger4breadboardPC.ino
@@ -1,5 +1,8 @@
 const char ADDR[] = {53, 51, 49, 47, 45, 43, 41, 39, 37, 35, 33, 31, 29, 27, 25, 23};
 const char DATA[] = {52, 50, 48, 46, 44, 42, 40, 38};
+const char D_PORTA[] = {36,34,32,30,28,26,24,22};
+const char D_PORTB[] = {6,5,4};
+
 #define CLOCK 2
 #define READ_WRITE 3
 
@@ -9,6 +12,12 @@ void setup() {
   }
   for(int i=0; i<8; i++) {
     pinMode(DATA[i], INPUT);
+  }
+  for(int i=0; i<8; i++) {
+    pinMode(D_PORTA[i], INPUT);
+  }
+  for(int i=0; i<3; i++) {
+    pinMode(D_PORTB[i], INPUT);
   }
   pinMode(CLOCK, INPUT);
   pinMode(READ_WRITE, INPUT);
@@ -34,8 +43,22 @@ void onClock() {
     data = (data << 1 ) + bit;
     Serial.print(bit);
   }
+  Serial.print("  -  ");
+  unsigned int d_portb = 0;
+  for(int i=0; i<3; i++) { 
+    int bit = digitalRead(D_PORTB[i]) ? 1 : 0;
+    d_portb = (d_portb << 1 ) + bit;
+    Serial.print(bit);
+  }
+  Serial.print("  -  ");
+  unsigned int d_porta = 0;
+  for(int i=0; i<8; i++) { 
+    int bit = digitalRead(D_PORTA[i]) ? 1 : 0;
+    d_porta = (d_porta << 1 ) + bit;
+    Serial.print(bit);
+  }
 
-  sprintf(output, "  %04x %c %02x", address, digitalRead(READ_WRITE) ? 'r':'W', data);
+  sprintf(output, "  %04x %c %02x ", address, digitalRead(READ_WRITE) ? 'r':'W', data);
 
   Serial.println(output);
 }
--- blink.s
+++ blink.s
@@ -1,7 +1,6 @@
 PORTB = $6000
-DDRB = $6002
-
 PORTA = $6001
+DDRB = $6002
 DDRA = $6003
 
 E  = %10000000
@@ -11,49 +10,82 @@ RS = %00100000
   .org $8000
 
 reset:
-  lda #%11111111 ; SET ALL PINS ON PORT B TO OUTPUT
+  lda #%11111111 ; Set all pins on port B to output
   sta DDRB
-  lda #%11100000 ; SET TOP 3 PIN ON PORT A TO OUTPUT
+
+  lda #%11100000 ; Set top 3 pins on port A to output
   sta DDRA
 
-  lda #%00111000 ; SET 8-bit MODE; 2-line DISPLAY; 5x8 FONT
-  sta PORTB
-  lda #0	 ; CLEAR RS/RW/E bits
-  sta PORTA
-  lda #E	 ; TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
-  sta PORTA
-  lda #0         ; CLEAR RS/RW/E bits
-  sta PORTA
+  lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
+  jsr lcd_instruction
 
-  lda #%00001110 ; SET DISPLAY ON, SET CURSOR ON, SET CURSOR-BLINKING OFF 
-  sta PORTB
-  lda #0         ; CLEAR RS/RW/E bits
-  sta PORTA
-  lda #E         ; TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
-  sta PORTA
-  lda #0         ; CLEAR RS/RW/E bits
-  sta PORTA
+  lda #%00001110 ; Display on; cursor on; blink off
+  jsr lcd_instruction
 
-  lda #%00000110 ; SET DISPLAY TO SHIFT THE CURSOR TO THE RIGHT, SET TO NOT SHIFT DISPLAY
-  sta PORTB
-  lda #0         ; CLEAR RS/RW/E bits
-  sta PORTA
-  lda #E         ; TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
-  sta PORTA
-  lda #0	 ; CLEAR RS/RW/E bits
-  sta PORTA
+  lda #%00000110 ; Increment and shift cursor; don't shift display
+  jsr lcd_instruction
 
-  lda #"H"       ; WRITE H
-  sta PORTB
-  lda #RS        ; SET RS AND CLEAR RW/E bits
-  sta PORTA
-  lda #(RS | E)  ; KEEP RS SET AND TOGGLE ENABLE BIT FOR SENDING THE INSTRUCTION
-  sta PORTA
-  lda #RS        ; SET RS AND CLEAR RW/E bits
-  sta PORTA
+  lda #"H"
+  jsr print_char
+
+  lda #"e"
+  jsr print_char
+
+  lda #"l"
+  jsr print_char
+
+  lda #"l"
+  jsr print_char
+
+  lda #"o"
+  jsr print_char
+
+  lda #","
+  jsr print_char
+
+  lda #" "
+  jsr print_char
+
+  lda #"w"
+  jsr print_char
+
+  lda #"o"
+  jsr print_char
+
+  lda #"r"
+  jsr print_char
+
+  lda #"l"
+  jsr print_char
+
+  lda #"d"
+  jsr print_char
+
+  lda #"!"
+  jsr print_char
 
 loop:
   jmp loop
+
+lcd_instruction:
+  sta PORTB
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  lda #E         ; Set E bit to send instruction
+  sta PORTA
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  rts
+
+print_char:
+  sta PORTB
+  lda #RS         ; Set RS; Clear RW/E bits
+  sta PORTA
+  lda #(RS | E)   ; Set E bit to send instruction
+  sta PORTA
+  lda #RS         ; Clear E bits
+  sta PORTA
+  rts
 
   .org $fffc
   .word reset
